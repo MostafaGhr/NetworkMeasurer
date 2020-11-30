10f718d6b57fda35db86e0f22efe7f5e532ebcdd
Mostafa <mostafa_gho@yahoo.com>
2020-11-30 19:51:21 +0330

    scheduler added

--- app.js
+++ app.js
@@ -1,65 +1,127 @@
-const fs = require('fs');
-let config = JSON.parse(fs.readFileSync('config.json'));
+const { json } = require("express");
+const fs = require("fs");
+let config = JSON.parse(fs.readFileSync("config.json"));
 
-var mqtt = require('mqtt');
-const { pingPublish } = require('./myTools/networkTools');
-const networkTools = require('./myTools/networkTools');
+var mqtt = require("mqtt");
+const { pingPublish } = require("./myTools/networkTools");
+const networkTools = require("./myTools/networkTools");
 
-// topics 
-statusTopic = "status"
-iperfTopic = "iperf"
-pingTopic = "ping"
+// topics
+settingTopic = "setting";
+statusTopic = "status";
+iperfTopic = "iperf";
+pingTopic = "ping";
 
-
-var client = mqtt.connect('mqtt://' + config.brokerAdress, {
-  will:{ topic:statusTopic + "/" + config.clientID, payload:"off"}
-})
-
+var client = mqtt.connect("mqtt://" + config.brokerAdress, {
+  will: { topic: statusTopic + "/" + config.clientID, payload: "off" },
+});
 
 // start default tasks
 networkTools.tracer(config.traceDestination, config.traceDepth);
 
-setInterval(() => {
-  networkTools.pingPublish(trace_list, (err, ip, date, res) => {
-    client.publish(pingTopic + "/" + config.clientID, JSON.stringify({"ip":ip, "date":date, "time":res.time}))
+payloadSize = config.pingPayloadSize;
+sourceAddress = config.pingSourceAddress;
+
+pingInterval = setInterval(() => {
+  if (config.pingSourceAddress == "random") {
+    sourceAddress =
+      Math.floor(Math.random() * 255) +
+      1 +
+      "." +
+      Math.floor(Math.random() * 255) +
+      "." +
+      Math.floor(Math.random() * 255) +
+      "." +
+      Math.floor(Math.random() * 255);
+  }
+  networkTools.pingPublish(trace_list, payloadSize, sourceAddress, (err, ip, date, res) => {
+    if (err) {
+      console.log(err);
+    } else {
+      client.publish(pingTopic + "/" + config.clientID, JSON.stringify({ ip: ip, date: date, time: res.time }));
+    }
   });
-}, 1000);
+}, config.pingInterval);
 
 // end start default tasks
 
-client.on('connect', function () {
+client.on("connect", function () {
   client.publish(statusTopic + "/" + config.clientID, "on");
   client.subscribe(config.clientID + "/#", function (err) {
     if (!err) {
-      console.log("subscribed to \"" + config.clientID + "/#\" successfully");
+      console.log('subscribed to "' + config.clientID + '/#" successfully');
     }
   });
   client.subscribe(statusTopic, function (err) {
     if (!err) {
-      console.log("subscribed to \"" + statusTopic + "/#\" successfully");
+      console.log('subscribed to "' + statusTopic + '/#" successfully');
     }
   });
 });
 
-client.on('message', function (topic, message) {
+client.on("message", function (topic, message) {
   parsedTopic = topic.split("/");
   console.log(parsedTopic);
 
-  // console.log(topic.toString())
-  // console.log(message.toString())
+  // console.log(topic.toString());
+  // console.log(message.toString());
 
   if (parsedTopic[0] == config.clientID) {
     if (parsedTopic[1] == "traceReset") {
       networkTools.tracer(config.traceDestination, config.traceDepth, (err, ip, res) => {
         if (!err) {
-          client.publish(iperfTopic + "/" + config.clientID + "/" + time, output)
+          client.publish(iperfTopic + "/" + config.clientID + "/" + time, output);
         }
       });
     }
     if (parsedTopic[1] == "startTest") {
-      networkTools.iperf_handler(config.iperfDirectory, config.iperfServerIP, config.iperfServerPort, (err, time, output) => {
+      networkTools.iperf_handler(config.iperfServerIP, config.iperfServerPort, (err, time, output) => {
         if (!err) {
-          client.publish(iperfTopic + "/" + config.clientID + "/" + time, output)
+          client.publish(iperfTopic + "/" + config.clientID + "/" + time, output);
+        }
+      });
+    }
+    if (parsedTopic[1] == "ping") {
+      if (message.toString() == "on") {
+        if (!pingInterval) {
+          pingInterval = setInterval(() => {
+            if (config.pingSourceAddress == "random") {
+              sourceAddress =
+                Math.floor(Math.random() * 255) +
+                1 +
+                "." +
+                Math.floor(Math.random() * 255) +
+                "." +
+                Math.floor(Math.random() * 255) +
+                "." +
+                Math.floor(Math.random() * 255);
+            }
+            networkTools.pingPublish(trace_list, payloadSize, sourceAddress, (err, ip, date, res) => {
+              if (err) {
+                console.log(err);
+              } else {
+                client.publish(
+                  pingTopic + "/" + config.clientID,
+                  JSON.stringify({ ip: ip, date: date, time: res.time })
+                );
+              }
+            });
+          }, config.pingInterval);
+        }
+      }
+      if (message.toString() == "off") {
+        clearInterval(pingInterval);
+        pingInterval = false;
+      }
+    }
+    if (parsedTopic[1] == settingTopic) {
+      settingJson = JSON.parse(message.toString());
+      for (var attr in settingJson) {
+        config[attr] = settingJson[attr];
+      }
+      fs.writeFile("config.json", JSON.stringify(config), (err) => {
+        if (err) {
+          console.log(err);
         }
       });
     }
@@ -74,10 +136,6 @@ client.on('message', function (topic, message) {
   }
 });
 
-
 client.on("close", () => {
   client.publish(statusTopic + config.clientID, "off");
 });
-
-
-
--- myTools/networkTools.js
+++ myTools/networkTools.js
@@ -1,135 +1,136 @@
 // iperf header
-const { exec } = require('child_process');
+const { exec } = require("child_process");
 // end iperf header
 
 // ping header
-var ping = require('ping');
-const fs = require('fs');
-const { Parser } = require('json2csv');
+var ping = require("ping");
+const fs = require("fs");
+const { Parser } = require("json2csv");
 
 let ping_res = {};
 // end ping header
 
 //trace header
-const Traceroute = require('nodejs-traceroute');
+const Traceroute = require("nodejs-traceroute");
 
-trace_list=[]
+trace_list = [];
 //end trace header
 
+function iperf_handler(iperf_server_address, iperf_port_address, callback) {
+  let date_ob = Date();
+  date_ob = date_ob.split(" ").join("_");
+  date_ob = date_ob.replace("+", "_plus");
+  date_ob = date_ob.replace("-", "_neg");
+  date_ob = date_ob.replace("(", "");
+  date_ob = date_ob.replace(")", "");
+  // exec("mkdir " + iperf_path);
 
-function iperf_handler(iperf_path, iperf_server_address, iperf_port_address, callback) {
-    let date_ob = Date();
-    date_ob = date_ob.split(" ").join("_");
-    date_ob = date_ob.replace("+", "_plus");
-    date_ob = date_ob.replace("-", "_neg");
-    date_ob = date_ob.replace("(", "");
-    date_ob = date_ob.replace(")", "");
-    exec("mkdir " + iperf_path);
+  // command to execute!
+  let comm = "iperf3 -c " + iperf_server_address + " -p " + iperf_port_address + " -J";
 
-    // command to execute!
-    let comm = 'iperf3 -c ' + iperf_server_address + " -p " + iperf_port_address + " -J";
+  console.log(comm);
 
-    console.log(comm);
-
-    exec(comm, (err, stdout, stderr) => {
-        if (err) {
-            console.log(err);
-            console.log(stdout);
-            console.log(stderr);   
-            return callback(err, null, stdout);
-        }
-        else {
-            callback(null, date_ob, stdout);
-        }
-    })
-
+  exec(comm, (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+      console.log(stdout);
+      console.log(stderr);
+      return callback(err, null, stdout);
+    } else {
+      callback(null, date_ob, stdout);
+    }
+  });
 }
 
 function pingToLocalCSV(ipList, savePath) {
-    ipList.forEach(ip => {
-        ping.promise.probe(ip)
-                .then((res) => {                    
-                    if (ping_res[ip] == undefined){
-                        ping_res[ip] = []
-                    }
-                    let date_ob = Date();
-                    fs.access(savePath + ip + ".csv", fs.constants.F_OK | fs.constants.W_OK, (err) => {
-                        if (err) {
-                            const json2csvParser = new Parser({ header: true });
-                            csver = json2csvParser.parse({
-                                "dest":ip,
-                                "date":date_ob,
-                                "rtt":res.time
-                            });
-                            fs.writeFile(savePath + ip.toString() + ".csv", csver + "\n\r", (err) => {
-                                if (err) {
-                                    console.log(err);
-                                }
-                            });
-                        } else {
-                            const json2csvParser = new Parser({ header: false });
-                            csver = json2csvParser.parse({
-                                "dest":ip,
-                                "date":date_ob,
-                                "rtt":res.time
-                            });
-                            fs.appendFile(savePath + ip.toString() + ".csv", csver + "\r\n", (err) => {
-                                if (err) {
-                                    console.log(err);
-                                }
-                            });
-                        }
-                    });
-                })
-                .catch(e => {
-                    console.log(e)
-                })
-    })
+  ipList.forEach((ip) => {
+    ping.promise
+      .probe(ip, {
+        packetSize: 70,
+      })
+      .then((res) => {
+        if (ping_res[ip] == undefined) {
+          ping_res[ip] = [];
+        }
+        let date_ob = Date();
+        fs.access(savePath + ip + ".csv", fs.constants.F_OK | fs.constants.W_OK, (err) => {
+          if (err) {
+            const json2csvParser = new Parser({ header: true });
+            csver = json2csvParser.parse({
+              dest: ip,
+              date: date_ob,
+              rtt: res.time,
+            });
+            fs.writeFile(savePath + ip.toString() + ".csv", csver + "\n\r", (err) => {
+              if (err) {
+                console.log(err);
+              }
+            });
+          } else {
+            const json2csvParser = new Parser({ header: false });
+            csver = json2csvParser.parse({
+              dest: ip,
+              date: date_ob,
+              rtt: res.time,
+            });
+            fs.appendFile(savePath + ip.toString() + ".csv", csver + "\r\n", (err) => {
+              if (err) {
+                console.log(err);
+              }
+            });
+          }
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
 }
 
-
-function pingPublish(ipList, callback) {
-    ipList.forEach(ip => {
-        ping.promise.probe(ip)
-            .then((res) => {                
-                let date_ob = Date();
-                callback(null, ip, date_ob, res);
-            })
-            .catch(e => {
-                callback(e);
-            })
-    })
+function pingPublish(ipList, packetSize, sourceAddr, callback) {
+  ipList.forEach((ip) => {
+    ping.promise
+      .probe(ip, {
+        packetSize: packetSize,
+        sourceAddr: sourceAddr,
+      })
+      .then((res) => {
+        let date_ob = Date();
+        callback(null, ip, date_ob, res);
+      })
+      .catch((e) => {
+        callback(e);
+      });
+  });
 }
-
 
 function tracer(destination, depth) {
-    trace_list=[]
-    i = 0;
-    try {
-        const tracer = new Traceroute();
-        tracer
-            .on('pid', (pid) => {
-                // console.log(`pid: ${pid}`);
-            })
-            .on('destination', (destination) => {
-                // console.log(`destination: ${destination}`);
-            })
-            .on('hop', (hop) => {
-                if (i < depth) {
-                    i += 1;
-                    trace_list.push(hop.ip);
-                }
-                // console.log(`hop: ${JSON.stringify(hop)}`);
-            })
-            .on('close', (code) => {
-                // console.log(`close: code ${code}`);
-            });
-    
-        tracer.trace(destination);
-    } catch (ex) {
-        console.log(ex);
-    }
+  trace_list = [];
+  i = 0;
+  try {
+    const tracer = new Traceroute();
+    tracer
+      .on("pid", (pid) => {
+        // console.log(`pid: ${pid}`);
+      })
+      .on("destination", (destination) => {
+        // console.log(`destination: ${destination}`);
+      })
+      .on("hop", (hop) => {
+        if (i < depth) {
+          i += 1;
+          trace_list.push(hop.ip);
+        }
+        // console.log(`hop: ${JSON.stringify(hop)}`);
+      })
+      .on("close", (code) => {
+        // console.log(`close: code ${code}`);
+      });
+
+    tracer.trace(destination);
+  } catch (ex) {
+    console.log(ex);
+  }
 }
 
-
-module.exports = {iperf_handler, pingToLocalCSV, pingPublish, tracer}
+module.exports = { iperf_handler, pingPublish, tracer };
